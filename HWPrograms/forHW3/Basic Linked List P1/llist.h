1c0ed5bb34abd1e674b1a2d49486263398e9f813
AxialLP7 <axial.jc@gmail.com>
2015-03-19 16:49:54 -0700

    Copied files into correct directories and fixed target error with Xcode. Turns out I had the files in a nonexistent folder. The solution was to remove the folder and add the files back in manually.

---
+++ HWPrograms/forHW3/Basic Linked List P1/llist.h
@@ -0,0 +1,64 @@
+
+//****************************************************************************
+//  CS311 Linked - by Rika for those who could not get HW3 to work
+//  Header File  llist.h
+//****************************************************************************
+
+/* ====================================================
+ 
+ HW3P1 llist
+ Your name: Jer Cherng Law
+ Complier:  Xcode g++ 4.2.1
+ File type: Implementation file for linked lists.
+ 
+ //===================================================== */
+
+#include <iostream>
+using namespace std;
+
+typedef int el_t;
+
+// list node is defined here as a struct Node
+struct Node
+{
+  el_t Elem;   // elem is the element stored
+  Node *Next;  // next is the pointer to the next node
+};
+
+// I could have done class Node and add the data members under public
+// but it will use too much space
+
+class llist
+{
+  
+ protected:
+  
+  Node *Front;       // front  pointer 
+  Node *Rear;        // rear   pointer
+  int  Count;        // counter for the number of elements
+  
+ public:
+  
+  class Underflow{};
+  class OutOfRange {};
+    
+  llist ();                       // constructor  
+  ~llist();                       // destructor
+  
+  bool isEmpty();
+    
+  void displayAll();
+    
+  void addRear(el_t NewNum);
+    
+  void deleteFront(el_t& OldNum);
+    
+  void addFront(el_t NewNum);
+    
+  void deleteRear(el_t& OldNum);
+    
+  void deleteIth(int I, el_t& OldNum);
+    
+  void addbeforeIth(int I, el_t newNum);
+    
+};
